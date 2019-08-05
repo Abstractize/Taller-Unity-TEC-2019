3e28a06b6bdfdb4783b711c1a4091f9d03977a62
Victor Castrillo <victor.castrillo.99@gmail.com>
2019-08-04 17:59:54 -0700

    [Add] Driving and Camera scripts
    CarDriving is added as a component of an empty object and that object must have a child with a collider.

---
+++ Taller Unity/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Transform of the object to look at and the camera
+    public Transform lookAt;
+    public Transform camTransform;
+
+    // Camera and vector of the distance between camera and object
+    private Camera cam;
+    private Vector3 distance;
+    
+    private void Awake()
+    {
+        lookAt = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    
+    void Start()
+    {
+        camTransform = transform;
+        cam = Camera.main;
+        distance = new Vector3(0.0f, 5.0f, -10.0f);
+    }
+    
+    void LateUpdate()
+    {
+        camTransform.position = lookAt.position + lookAt.rotation * distance;
+        camTransform.LookAt(lookAt.position);
+    }
+}
---
+++ Taller Unity/Assets/Scripts/CarDriving.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarDriving : MonoBehaviour
+{
+    // Speed for movement
+    public float movementSpeed = 20.0f;
+    public float rotationSpeed = 0.75f;
+
+    // GameObject's needed
+    public GameObject car;
+    public Collider coll;
+
+    // Flags for drifting
+    private bool driftDir = true;       // True = left || False = right
+    private bool drift = false;
+
+    private void Start()
+    {
+        coll = GetComponentInChildren<Collider>();
+    }
+
+    private void Update()
+    {
+        // Drifting Flag
+        if (Input.GetKeyDown(KeyCode.Space) && Input.GetKey(KeyCode.A) && !drift && IsGrounded())
+        {
+            drift = true;
+            driftDir = true;
+            car.transform.Rotate(0.0f, -40.0f, 0.0f);
+            car.GetComponent<Rigidbody>().velocity += new Vector3(0.0f, 3.0f, 0.0f);
+        } else if (Input.GetKeyDown(KeyCode.Space) && Input.GetKey(KeyCode.D) && IsGrounded())
+        {
+            drift = true;
+            driftDir = false;
+            car.transform.Rotate(0.0f, 40.0f, 0.0f);
+            car.GetComponent<Rigidbody>().velocity += new Vector3(0.0f, 3.0f, 0.0f);
+        }
+        if (Input.GetKeyUp(KeyCode.Space) && drift)
+        {
+            drift = false;
+            if (driftDir)
+            {
+                car.transform.Rotate(0.0f, 40.0f, 0.0f);
+            } else
+            {
+                car.transform.Rotate(0.0f, -40.0f, 0.0f);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Movement
+        if (!drift || (drift && !IsGrounded()))
+        {
+            // Movement and rotation (Normal)
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.Rotate(-transform.up * rotationSpeed);
+                }
+                else if (Input.GetKey(KeyCode.D))
+                {
+                    transform.Rotate(transform.up * rotationSpeed);
+                }
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(-Vector3.forward * movementSpeed * Time.deltaTime);
+
+                if (Input.GetKey(KeyCode.A))
+                {
+                    transform.Rotate(transform.up * rotationSpeed);
+                }
+                else if (Input.GetKey(KeyCode.D))
+                {
+                    transform.Rotate(-transform.up * rotationSpeed);
+                }
+            }
+        }
+        // Movement and rotation (Drifting)
+        else if (drift && IsGrounded())
+        {
+            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime / 1.75f);
+            if (driftDir)
+            {
+                transform.Rotate(-transform.up * rotationSpeed * 2f);
+            } else
+            {
+                transform.Rotate(transform.up * rotationSpeed * 2f);
+            }
+        }
+    }
+    
+    public bool IsGrounded()
+    {
+        bool hit = Physics.Raycast(coll.bounds.center, Vector3.down, coll.bounds.extents.y + 0.1f);
+        return hit;
+    }
+}
